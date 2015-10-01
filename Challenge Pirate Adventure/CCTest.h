8f751a8cf78195d9f2928f10b5d972c4b3b3c83d
Klappergebiss <tim2401@gmx.net>
2015-10-01 22:20:37 +0200

    added a test class

---
+++ Challenge Pirate Adventure/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Challenge Pirate Adventure
+//
+//  Created by Tim Engel on 01.10.15.
+//  Copyright © 2015 Tim Engel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Challenge Pirate Adventure/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Challenge Pirate Adventure
+//
+//  Created by Tim Engel on 01.10.15.
+//  Copyright © 2015 Tim Engel. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
